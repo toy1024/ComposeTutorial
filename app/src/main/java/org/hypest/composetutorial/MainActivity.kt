28e5768c2ef5abb7790b4e1356eee623506f962c
Toy1024 <stefanostogoulidis+toy@gmail.com>
2025-01-23 22:54:32 +0200

    Adds some Material theming

--- app/src/main/java/org/hypest/composetutorial/MainActivity.kt
+++ app/src/main/java/org/hypest/composetutorial/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,6 +21,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import org.hypest.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,13 +42,15 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier
                     .size(40.dp)
                     .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
 
             Spacer(modifier = Modifier.width(8.dp))
 
             Column {
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(text = msg.author)
+                Text(text = msg.author,
+                    color = MaterialTheme.colorScheme.secondary)
                 Spacer(modifier = Modifier.height(2.dp))
                 Text(text = msg.body)
                 Spacer(modifier = Modifier.height(4.dp))
@@ -55,6 +61,10 @@ class MainActivity : ComponentActivity() {
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(Message("Stefanos Togoulidis", "Hello there!"))
+        ComposeTutorialTheme {
+            Surface {
+                MessageCard(Message("Stefanos Togoulidis", "Hello there!"))
+            }
+        }
     }
 }
